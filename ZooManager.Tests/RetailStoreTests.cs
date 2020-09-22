5f97bdde6af3c03a4082e2cab1d8d4913d3d3607
Wayne Schroder <wschroder@outlook.com>
2020-09-21 23:31:51 -0400

    Update packages

---
+++ ZooManager.Tests/RetailStoreTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZooManager.Entities;
+using ZooManager.Factories;
+
+namespace ZooManager.Tests
+{
+    [TestClass]
+    public class RetailStoreTests
+    {
+        public void GetItem()
+        {
+            var sut = RetailStoreFactory.CreateGiftShop();
+
+            RetailItem item = sut.GetItem(deptName: "Snacks", itemName: "Ice Cream");
+
+            Assert.IsNotNull(item);
+        }
+    }
+}
